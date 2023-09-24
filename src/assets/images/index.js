519735b30b96c74b9b24367d5be482dceb21cda8
mollgerman <germandmoll2004@gmail.com>
2023-09-24 14:19:59 -0300

    fix

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -17,8 +17,8 @@ import shoe5 from './shoe7.svg'
 import shoe8 from './shoe8.svg'
 
 import thumbnailBackground from './thumbnail-background.svg'
-import thumbnailShoe1 from './thumbnail-shoe1.svg'
-import thumbnailShoe2 from './thumbnail-shoe2.svg'
+import thumbnailShoe2 from './thumbnail-shoe1.svg'
+import thumbnailShoe1 from './thumbnail-shoe2.svg'
 import thumbnailShoe3 from './thumbnail-shoe3.svg'
 
 import nike from './nike.png'
